61e65c5fc33696e326530fb53c84f0a4bcc19538
Bryce Pauken <brycepauken@Bryces-MacBook-Pro.local>
2014-04-05 08:41:52 -0700

    Reset back to blank project for another full revision

--- BCP-Mobile/BCP-Mobile/BCPAppDelegate.h
+++ BCP-Mobile/BCP-Mobile/BCPAppDelegate.h
@@ -2,12 +2,11 @@
 //  BCPAppDelegate.h
 //  BCP-Mobile
 //
-//  Created by Bryce Pauken on 11/17/13.
-//  Copyright (c) 2013 Kingfish. All rights reserved.
+//  Created by Bryce Pauken on 4/5/14.
+//  Copyright (c) 2014 sobryapps. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "BCPViewController.h"
 
 @interface BCPAppDelegate : UIResponder <UIApplicationDelegate>
 
--- BCP-Mobile/BCP-Mobile/main.m
+++ BCP-Mobile/BCP-Mobile/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  BCP-Mobile
 //
-//  Created by Bryce Pauken on 11/17/13.
-//  Copyright (c) 2013 Kingfish. All rights reserved.
+//  Created by Bryce Pauken on 4/5/14.
+//  Copyright (c) 2014 sobryapps. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
